e5b79ab10a16c33b61693fd842a96ed1904d146a
Onalerona Zwane <onzwanejhb024@student.wethinkcode.co.za>
2025-11-17 14:06:49 +0200

    Clear cart done

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -13,6 +13,7 @@ import { OnInit } from '@angular/core';
 export class CartViewComponent implements OnInit {
 
   cartItems : Product[] = []
+  totalPrice: number = 0
 
   constructor(private cartService: CartService){
 
@@ -21,7 +22,22 @@ export class CartViewComponent implements OnInit {
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data
+      this.totalPrice = this.getTotalPrice();
     })
+
+  }
+
+  getTotalPrice(): number{
+    let total = 0;
+    for(let item of this.cartItems){
+      total += item.price;
+    }
+
+    return total
+  }
+
+  clearCart(): void{
+    this.cartService.clearCart().subscribe()
   }
 
 }
